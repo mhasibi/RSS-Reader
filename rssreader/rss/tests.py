21aa0669568dde9286e9ce70fab595ea83b49fbb
mojan.hasibi@gmail.com <mojan.hasibi@gmail.com>
2019-02-06 22:06:31 -0800

    testcases started

--- rssreader/rss/tests.py
+++ rssreader/rss/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class RssIndexViewTests(TestCase):
+    def test_no_feed(self):
+        pass
+
+    def test_user_feed(self):
+        pass
